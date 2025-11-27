4304f11414de4cda4efeea383737f7d1c1862d58
kotami <145974605+xkotami@users.noreply.github.com>
2025-11-27 12:16:00 +0000

    hotfix

--- src/commands/status.js
+++ src/commands/status.js
@@ -24,8 +24,11 @@ module.exports = {
         try {
             const response = await util.status(ip, port);
             console.log(response);
-            const playerNames = response.players.sample.map(player => player.name).join(", ");
-            const statusMessage = `**${ip}** is **online** 🟢\n- **Players**: ${playerNames ? playerNames : "None"}\n- **Version**: ${response.version.name} \n- **Ping**: ${response.roundTripLatency}\n- **MOTD**: ${response.motd.clean}`;
+            let playerNames = "None :(";
+            if (response.players.sample !== null) {
+                playerNames = response.players.sample.map(player => player.name).join(", ");
+            }
+            const statusMessage = `**${ip}** is **online** 🟢\n- **Players**: ${playerNames}\n- **Version**: ${response.version.name} \n- **Ping**: ${response.roundTripLatency}\n- **MOTD**: ${response.motd.clean}`;
             await interaction.editReply(statusMessage);
         } catch (error) {
             await interaction.editReply(`❌ Could not reach **${ip}**. Server might be offline HEEEEELP`);
